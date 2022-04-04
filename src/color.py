c7674817208adbbffa3a0eba38849ccbc14f012f
t3knical <t3chnicald3ath@gmail.com>
2022-04-03 21:16:07 -0400

    Update color.py

--- src/color.py
+++ src/color.py
@@ -1,52 +1,68 @@
 from enum import Enum
 from PIL import ImageColor
 
+
 class Color(Enum):
+    PINKISH_RED = {"id": 0, "hex": "#6D001A"}
     DARK_RED = {"id": 1, "hex": "#BE0039"}
     RED = {"id": 2, "hex": "#FF4500"}
     ORANGE = {"id": 3, "hex": "#FFA800"}
     YELLOW = {"id": 4, "hex": "#FFD635"}
+    BEIGE = {"id": 5, "hex": "#FFF8B8"}
     DARK_GREEN = {"id": 6, "hex": "#00A368"}
     GREEN = {"id": 7, "hex": "#00CC78"}
     LIGHT_GREEN = {"id": 8, "hex": "#7EED56"}
     DARK_TEAL = {"id": 9, "hex": "#00756F"}
     LIGHT_TEAL = {"id": 10, "hex": "#009EAA"}
+    SATURATED_TEAL = {"id": 11, "hex": "#00CCC0"}
     DARK_BLUE = {"id": 12, "hex": "#2450A4"}
     BLUE = {"id": 13, "hex": "#3690EA"}
     CYAN = {"id": 14, "hex": "#51E9F4"}
     TURQUOISE = {"id": 15, "hex": "#493AC1"}
     BLUEISH_PURPLE = {"id": 16, "hex": "#6A5CFF"}
+    # sry bout that name, they keep expanding the color palette
+    LIGHT_BLUEISH_PURPLE = {"id": 17, "hex": "#94B3FF"}
     DARK_PURPLE = {"id": 18, "hex": "#811E9F"}
     PURPLE = {"id": 19, "hex": "#B44AC0"}
+    LIGHT_PURPLE = {"id": 20, "hex": "#E4ABFF"}
+    SATURATED_PURPLE = {"id": 21, "hex": "#DE107F"}
     DARK_PINK = {"id": 22, "hex": "#FF3881"}
     LIGHT_PINK = {"id": 23, "hex": "#FF99AA"}
     DARK_BROWN = {"id": 24, "hex": "#6D482F"}
     BROWN = {"id": 25, "hex": "#9C6926"}
+    LIGHT_BROWN = {"id": 26, "hex": "#FFB470"}
     BLACK = {"id": 27, "hex": "#000000"}
     DARK_GREY = {"id": 28, "hex": "#515252"}
     GREY = {"id": 29, "hex": "#898D90"}
     LIGHT_GREY = {"id": 30, "hex": "#D4D7D9"}
     WHITE = {"id": 31, "hex": "#FFFFFF"}
 
+
+rgb_to_color = {}
 # generate rgb values for all colors
 for color in Color:
     color.value["rgb"] = ImageColor.getcolor(color.value["hex"], "RGB")
+    rgb_to_color.update({color.value["rgb"]: color})
 
 
 """
 Returns the color object based on the given rgb tuple
 """
-def get_matching_color(rgb) -> Color:
-    for color in Color:
-        if color.value["rgb"] == rgb:
-            return color
 
-    # print("Color not found:", rgb)
-    return None
+
+def get_matching_color(rgb) -> Color:
+    color = rgb_to_color.get(rgb)
+    if color is None:
+        # print("Color not found:", rgb)
+        return None
+    return color
+
 
 """
 Returns the color object based on a given place color index
 """
+
+
 def get_color_from_index(index) -> Color:
     for color in Color:
         if color.value["id"] == index:
@@ -65,8 +81,11 @@ def get_color_from_index(index) -> Color:
 #             closest_color = color
 #     return closest_color
 
+
 """
 Get the closest color available on place to any color for converting any image to a template
 """
+
+
 def get_closest_color(r, g, b) -> Color:
     return min(list(Color), key=lambda color: (r - color.value["rgb"][0]) ** 2 + (g - color.value["rgb"][1]) ** 2 + (b - color.value["rgb"][2]) ** 2)
