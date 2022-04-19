466bcabc1990c5694affe1d019bdb58636191ca5
t3knical <t3chnicald3ath@gmail.com>
2022-04-19 05:47:30 -0400

    Update userScript.Canada.js

--- Templates/Canada/userScript.Canada.js
+++ Templates/Canada/userScript.Canada.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Tekz Pixel Bot v2.0.1
+// @name         Tekz Pixel Bot v2.0.2
 // @namespace    https://github.com/t3knical/rplace
-// @version      2.0.1
+// @version      2.0.2
 // @description  overlay for r/place, For Canada!
 // @author       Sir Teknical
 // @match        http://place.cslabs.clarkson.edu/*
@@ -13,8 +13,8 @@
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @updateURL    https://raw.githubusercontent.com/t3knical/rplace/main/Templates/Canada/userScript.Canada.js
-// @downloadURL  https://raw.githubusercontent.com/t3knical/rplace/main/Templates/Canada/userScript.Canada.js
+// @updateURL    https://raw.githubusercontent.com/t3knical/rplace/main/Templates/Anime/userScript.js
+// @downloadURL  https://raw.githubusercontent.com/t3knical/rplace/main/Templates/Anime/userScript.js
 // ==/UserScript==
 
 const DEFAULT_TOAST_DURATION_MS = 10000;
@@ -32,16 +32,28 @@ var order3 = undefined;
 
 var accessToken;
 
-var currentOrderCanvas0;
-var currentOrderCtx0;
-var currentOrderCanvas1;
-var currentOrderCtx1;
-var currentOrderCanvas2;
-var currentOrderCtx2;
-var currentOrderCanvas3;
-var currentOrderCtx3;
+var currentOrderCanvas0 = undefined;;
+var currentOrderCtx0 = undefined;;
+var currentOrderCanvas1 = undefined;;
+var currentOrderCtx1 = undefined;;
+var currentOrderCanvas2 = undefined;;
+var currentOrderCtx2 = undefined;;
+var currentOrderCanvas3 = undefined;;
+var currentOrderCtx3 = undefined;;
 
-var currentPlaceCanvas;
+var rgbaOrder0 = undefined;;
+var rgbaOrder1 = undefined;;
+var rgbaOrder2 = undefined;;
+var rgbaOrder3 = undefined;;
+
+var work0 = undefined;;
+var work1 = undefined;;
+var work2 = undefined;;
+var work3 = undefined;;
+
+var rgbaCanvas = undefined;;
+
+var currentPlaceCanvas = undefined;
 
 var cnc_url0 = 'https://raw.githubusercontent.com/t3knical/rplace/main/Templates/Canada/full_canvas_template_canada_flag.png';
 var cnc_url1 = 'https://raw.githubusercontent.com/t3knical/rplace/main/Templates/Canada/full_canvas_template_canada_full.png';
@@ -68,6 +80,14 @@ async function attemptPlace() {
         setTimeout(attemptPlace, 1000) // probeer opnieuw in 2sec.
         return
     }
+    if (rgbaOrder0 == undefined || rgbaOrder1 == undefined || rgbaOrder2 == undefined || rgbaOrder3 == undefined) {
+        rgbaOrder0 = currentOrderCtx0.getImageData(0, 0, 2000, 2000).data
+        rgbaOrder1 = currentOrderCtx1.getImageData(0, 0, 2000, 2000).data
+        rgbaOrder2 = currentOrderCtx2.getImageData(0, 0, 2000, 2000).data
+        rgbaOrder3 = currentOrderCtx3.getImageData(0, 0, 2000, 2000).data
+        setTimeout(attemptPlace, 1000) // probeer opnieuw in 2sec.
+        return
+    }
     var ctx1
     try {
         ctx1 = await c
@@ -82,82 +102,97 @@ async function attemptPlace() {
         return
     }
 
-    const rgbaOrder0 = currentOrderCtx0.getImageData(0, 0, 2000, 2000).data
-    const rgbaOrder1 = currentOrderCtx1.getImageData(0, 0, 2000, 2000).data
-    const rgbaOrder2 = currentOrderCtx2.getImageData(0, 0, 2000, 2000).data
-    const rgbaOrder3 = currentOrderCtx3.getImageData(0, 0, 2000, 2000).data
-
-    const rgbaCanvas = ctx1.getImageData(0, 0, 2000, 2000).data
-
-    const work0 = getPendingWork(order0, rgbaOrder0, rgbaCanvas)
-    const work1 = getPendingWork(order1, rgbaOrder1, rgbaCanvas)
-    const work2 = getPendingWork(order2, rgbaOrder2, rgbaCanvas)
-    const work3 = getPendingWork(order3, rgbaOrder3, rgbaCanvas)
-
-    if (work0.length === 0 && work1.length === 0 && work2.length === 0 && work3.length === 0) {
-        Toastify({
-            text: `All pixels are already in the right place for all imgs! Try again in 2.5 sec...`,
-            duration: 5000,
-        }).showToast()
-        setTimeout(attemptPlace, 2500) // Try again in 2.5sec.
-        return
-    }
-
-    var percentComplete;
-    var workRemaining;
-    var idx;
-    var i;
-    var x1;
-    var y1;
-    var hex;
-
-    if (work0.length > 0) {
-        percentComplete = 100 - Math.ceil((work0.length * 100) / order0.length)
-        workRemaining = work0.length
-        idx = Math.floor(Math.random() * work0.length)
-        i = work0[idx]
-        x1 = i % 2000
-        y1 = Math.floor(i / 2000)
-        hex = rgbaOrderToHex(i, rgbaOrder0)
-    }
-    else if (work1.length > 0) {
-        percentComplete = 100 - Math.ceil((work1.length * 100) / order1.length)
-        workRemaining = work1.length
-        idx = Math.floor(Math.random() * work1.length)
-        i = work1[idx]
-        x1 = i % 2000
-        y1 = Math.floor(i / 2000)
-        hex = rgbaOrderToHex(i, rgbaOrder1)
-    }
-    else if (work2.length > 0) {
-        percentComplete = 100 - Math.ceil((work2.length * 100) / order2.length)
-        workRemaining = work2.length
-        idx = Math.floor(Math.random() * work2.length)
-        i = work2[idx]
-        x1 = i % 2000
-        y1 = Math.floor(i / 2000)
-        hex = rgbaOrderToHex(i, rgbaOrder2)
-    }
-    else if (work3.length > 0) {
-        percentComplete = 100 - Math.ceil((work3.length * 100) / order3.length)
-        workRemaining = work3.length
-        idx = Math.floor(Math.random() * work3.length)
-        i = work3[idx]
-        x1 = i % 2000
-        y1 = Math.floor(i / 2000)
-        hex = rgbaOrderToHex(i, rgbaOrder3)
-    }
-    // USED FOR MY LOCALHOST PROJECT
-    //ctx.fillStyle = hex;
-    //ctx.fillRect(Math.floor(x), Math.floor(y), pixelSize, pixelSize);
-    //socket.emit('load image', convertCanvasToImage(canvas).src);
-
-    //COOLDOWN = 0
 
     if (CD <= Date.now()) {
+        rgbaCanvas = ctx1.getImageData(0, 0, 2000, 2000).data
+        work0 = getPendingWork(order0, rgbaOrder0, rgbaCanvas)
+
+        if (work0.length === 0) {
+            work1 = getPendingWork(order1, rgbaOrder1, rgbaCanvas)
+            if (work1.length === 0) {
+                work2 = getPendingWork(order2, rgbaOrder2, rgbaCanvas)
+                if (work2.length === 0) {
+                    work3 = getPendingWork(order3, rgbaOrder3, rgbaCanvas)
+                }
+            }
+        }
+
+        if (work0.length === 0 && work1.length === 0 && work2.length === 0 && work3.length === 0) {
+            Toastify({
+                text: `All pixels are already in the right place for all imgs! Try again in 5 sec...`,
+                duration: 5000,
+            }).showToast()
+
+            setTimeout(attemptPlace, 5000) // Try again in 5 secs.
+            return
+        }
+
+        var percentComplete;
+        var workRemaining;
+        var idx;
+        var i;
+        var x1;
+        var y1;
+        var hex;
+        var currentcanvas;
+
+        if (work0.length > 0) {
+            percentComplete = 100 - Math.ceil((work0.length * 100) / order0.length)
+            workRemaining = work0.length
+            idx = Math.floor(Math.random() * work0.length)
+            i = work0[idx]
+            x1 = i % 2000
+            y1 = Math.floor(i / 2000)
+            hex = rgbaOrderToHex(i, rgbaOrder0)
+            Toastify({
+                text: `Found a misplace pixel @ ${x1}, ${y1}, on template 1!`,
+                duration: 5000,
+            }).showToast()
+        }
+        else if (work1.length > 0) {
+            percentComplete = 100 - Math.ceil((work1.length * 100) / order1.length)
+            workRemaining = work1.length
+            idx = Math.floor(Math.random() * work1.length)
+            i = work1[idx]
+            x1 = i % 2000
+            y1 = Math.floor(i / 2000)
+            hex = rgbaOrderToHex(i, rgbaOrder1)
+            Toastify({
+                text: `Found a misplace pixel @ ${x1}, ${y1}, on template 2!`,
+                duration: 5000,
+            }).showToast()
+        }
+        else if (work2.length > 0) {
+            percentComplete = 100 - Math.ceil((work2.length * 100) / order2.length)
+            workRemaining = work2.length
+            idx = Math.floor(Math.random() * work2.length)
+            i = work2[idx]
+            x1 = i % 2000
+            y1 = Math.floor(i / 2000)
+            hex = rgbaOrderToHex(i, rgbaOrder2)
+            Toastify({
+                text: `Found a misplace pixel @ ${x1}, ${y1}, on template 3!`,
+                duration: 5000,
+            }).showToast()
+        }
+        else if (work3.length > 0) {
+            percentComplete = 100 - Math.ceil((work3.length * 100) / order3.length)
+            workRemaining = work3.length
+            idx = Math.floor(Math.random() * work3.length)
+            i = work3[idx]
+            x1 = i % 2000
+            y1 = Math.floor(i / 2000)
+            hex = rgbaOrderToHex(i, rgbaOrder3)
+            Toastify({
+                text: `Found a misplace pixel @ ${x1}, ${y1}, on template 4!`,
+                duration: 5000,
+            }).showToast()
+        }
+
+
         autoput(x1, y1, hex)
         Toastify({ text: `Trying to place pixel ${x1}, ${y1}, color ${hex},${COLOR_MAPPINGS[hex]}... (${percentComplete}% complete, ${workRemaining} left)`, duration: DEFAULT_TOAST_DURATION_MS, }).showToast()
-        console.log(`Trying to place pixel ${x1}, ${y1}, color ${hex},${COLOR_MAPPINGS[hex]},... (${percentComplete}% complete, ${workRemaining} left)`)        
+        console.log(`Trying to place pixel ${x1}, ${y1}, color ${hex},${COLOR_MAPPINGS[hex]},... (${percentComplete}% complete, ${workRemaining} left)`)
     }
 
     setTimeout(attemptPlace, 250);
@@ -171,7 +206,7 @@ function autoput(X, Y, color) {
     y0 = Y;
     PEN = COLOR_MAPPINGS[color];
     set(Math.floor(x0), Math.floor(y0), PEN);
-    audios.cooldownStart.run();    
+    audios.cooldownStart.run();
     let a = new DataView(new Uint8Array(6).buffer);
     a.setUint8(0, 4);
     a.setUint32(1, Math.floor(x0) + Math.floor(y0) * WIDTH);
@@ -209,21 +244,18 @@ function getCanvasFromUrl(url, canvas1, x = 0, y = 0, clearCanvas = false) {
 };
 
 ws.onclose = () => {
-    if(CD != Infinity)return location.replace("https://rplace.tk");
+    if (CD != Infinity) return location.replace("https://rplace.tk");
     //Something went wrong...
     CD = 1e100;
 }
 
-setTimeout(() =>
-{
-  location.replace("https://rplace.tk");
+setTimeout(() => {
+    location.replace("https://rplace.tk");
 }, 2 * 60 * 60 * 1000);
 
 
-setInterval(() =>
-{
-    if(!load.buffer && CD >= 1e100 && CD != Infinity)
-    {
+setInterval(() => {
+    if (!load.buffer && CD >= 1e100 && CD != Infinity) {
         location.replace("https://rplace.tk");
     }
 }, 300);
@@ -296,7 +328,7 @@ let getPendingWork = (work, rgbaOrder, rgbaCanvas) => {
     currentOrderCtx0 = currentOrderCanvas0.getContext('2d');
     currentOrderCtx1 = currentOrderCanvas1.getContext('2d');
     currentOrderCtx2 = currentOrderCanvas2.getContext('2d');
-    currentOrderCtx3 = currentOrderCanvas2.getContext('2d');
+    currentOrderCtx3 = currentOrderCanvas3.getContext('2d');
 
     currentPlaceCanvas = document.createElement('CANVAS');
 
@@ -352,28 +384,28 @@ let getPendingWork = (work, rgbaOrder, rgbaCanvas) => {
     currentOrderCtx0 = await getCanvasFromUrl(cnc_url0, currentOrderCanvas0, 0, 0, true,)
     order0 = getRealWork(currentOrderCtx0.getImageData(0, 0, 2000, 2000).data)
     Toastify({
-        text: 'Done getting template1 from github, ${order0.length} pixels in total...',
+        text: `Done getting template1 from github, ${order0.length} pixels in total...`,
         duration: DEFAULT_TOAST_DURATION_MS,
     }).showToast()
 
     currentOrderCtx1 = await getCanvasFromUrl(cnc_url1, currentOrderCanvas1, 0, 0, true,)
     order1 = getRealWork(currentOrderCtx1.getImageData(0, 0, 2000, 2000).data)
     Toastify({
-        text: 'Done getting template1 from github, ${order1.length} pixels in total...',
+        text: `Done getting template2 from github, ${order1.length} pixels in total...`,
         duration: DEFAULT_TOAST_DURATION_MS,
     }).showToast()
 
     currentOrderCtx2 = await getCanvasFromUrl(cnc_url2, currentOrderCanvas2, 0, 0, true,)
     order2 = getRealWork(currentOrderCtx2.getImageData(0, 0, 2000, 2000).data)
     Toastify({
-        text: 'Done getting template1 from github, ${order2.length} pixels in total...',
+        text: `Done getting template3 from github, ${order2.length} pixels in total...`,
         duration: DEFAULT_TOAST_DURATION_MS,
     }).showToast()
 
     currentOrderCtx3 = await getCanvasFromUrl(cnc_url3, currentOrderCanvas3, 0, 0, true,)
     order3 = getRealWork(currentOrderCtx3.getImageData(0, 0, 2000, 2000).data)
     Toastify({
-        text: 'Done getting template1 from github, ${order3.length} pixels in total...',
+        text: `Done getting template4 from github, ${order3.length} pixels in total...`,
         duration: DEFAULT_TOAST_DURATION_MS,
     }).showToast()
 
